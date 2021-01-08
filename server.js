ccb5d6f1d49ce8b999a9c30c7e7a69b95137bc26
kizto86 <kingchuks@hotmail.com>
2021-01-08 23:16:17 +0100

    :fire:comment out Error handler for page not found due to heroku

--- server.js
+++ server.js
@@ -18,7 +18,7 @@ app.use("/questions", questionsRoute);
 
 //Error handler for page/route not found
 
-app.use((req, res, next) => {
+/*app.use((req, res, next) => {
   const error = new Error("Page Not found");
   error.status = 404;
   next(error);
@@ -32,7 +32,7 @@ app.use((error, req, res, next) => {
       message: error.message,
     },
   });
-});
+});*/
 
 //Setting up mongoose Connection
 
