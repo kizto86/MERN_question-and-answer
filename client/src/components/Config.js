edf1a19247bc2114e90709623a2b56104ae06e4f
kizto86 <kingchuks@hotmail.com>
2021-02-03 16:30:33 +0100

    :hammer:added dynamic routing for productionand development

--- client/src/components/Config.js
+++ client/src/components/Config.js
@@ -5,4 +5,4 @@ const inProduction = process.env.NODE_ENV === "production";
 exports.baseUrl =
   inDevelopment || inProduction
     ? `http://localhost:5000/questions/`
-    : "https://obscure-inlet-42322.herokuapp.com/";
+    : "https://obscure-inlet-42322.herokuapp.com/questions/";
