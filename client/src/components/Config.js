74b01c23fd81eb3dac660543147b3b649b1fdf66
kizto86 <kingchuks@hotmail.com>
2021-02-03 16:54:13 +0100

    :hammer:dynamic url updated

--- client/src/components/Config.js
+++ client/src/components/Config.js
@@ -1,8 +1,9 @@
-const inDevelopment = process.env.NODE_ENV === "development";
+//const inDevelopment = process.env.NODE_ENV === "development";
 
 const inProduction = process.env.NODE_ENV === "production";
 
-exports.baseUrl =
-  inDevelopment || inProduction
-    ? `http://localhost:5000/questions/`
-    : "https://obscure-inlet-42322.herokuapp.com/questions/";
+exports.baseUrl = inProduction
+  ? `http://localhost:5000/questions/`
+  : "https://obscure-inlet-42322.herokuapp.com/questions/";
+
+//connectionString: isProduction ? process.env.DATABASE_URL : connectionString,
