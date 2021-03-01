1d5c2007c267347e908c5fe69d10ed439ebe722c
kizto86 <kingchuks@hotmail.com>
2021-03-01 16:49:07 +0100

    :hammer:Nav bar updated

--- client/src/components/Nav.js
+++ client/src/components/Nav.js
@@ -7,7 +7,7 @@ export default class Nav extends Component {
   render() {
     return (
       <nav className="navbar navbar-dark bg-dark navbar-expand-lg">
-        <h1 className="navbar-brand">Question and Answer</h1>
+        <h1 className="navbar-brand">iHaveQuestions</h1>
         <div className="collapse navbar-collapse">
           <ul className="navbar-nav mr-auto">
             <li className="navbar-item">
